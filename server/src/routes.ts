4dedb979a28b70c3f8239d6b1cac9f976b02304c
Edvaldo Fendi <edvaldo.defendi@gmail.com>
2023-01-18 15:32:35 -0300

    detalhes do dia

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -32,5 +32,52 @@ export async function appRoutes(app: FastifyInstance) {
 
     })
 
+    app.get('/day', async (request) =>{
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        })
+
+        const { date } = getDayParams.parse(request.query)
+
+        const parsedDate = dayjs(date).startOf('day')
+        const weekDay = parsedDate.get('day')
+
+
+        console.log(date, weekDay)
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date,
+                },
+                weekDays:{
+                    some: {
+                        week_day: weekDay,
+                    }
+                }
+            }
+        })
+
+        const day = await prisma.day.findUnique({
+            where:{
+                date: parsedDate.toDate(),
+            },
+            include: {
+                DayHabits: true,
+            }
+        })
+
+        const completedHabits = day?.DayHabits.map(dayHabit => {
+            return dayHabit.habit_id
+        })
+
+        return {
+            possibleHabits,
+            completedHabits
+          
+        }
+    })
+
+
 }
 
