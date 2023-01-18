20362c0eb3e27256c6459652af7a112e0c14e2fe
Edvaldo Fendi <edvaldo.defendi@gmail.com>
2023-01-18 17:36:07 -0300

    classname funcionando

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,9 @@
 
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold})
@@ -14,22 +15,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Bem Vindo</Text>
+    <>
+      <Home />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent={true} />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    color: 'white',
-    fontFamily: 'Inter_700Bold'
-  }
-});
