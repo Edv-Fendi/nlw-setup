ec39f162613445abfd22b7eb202efd5de2b35473
Edvaldo Fendi <edvaldo.defendi@gmail.com>
2023-01-18 16:06:15 -0300

    Header

---
+++ web/src/components/Header.tsx
@@ -0,0 +1,24 @@
+import { Plus } from 'phosphor-react'
+
+import logoImage from '../assets/logo.svg';
+
+
+
+export function Header() {
+    return (
+
+        <div className='w-full max-w-3xl mx-auto flex items-center justify-between' >
+
+            <img src={logoImage} alt="Habits, imagem logo" />
+
+            <button type='button' className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:bg-violet-700'>
+                <Plus size={20} className="text-violet-500" />
+
+                Novo Hábito
+
+            </button>
+            
+        </div >
+    )
+
+} 
