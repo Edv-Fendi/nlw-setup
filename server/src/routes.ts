5b75af3b8125180c61a3b13ae33538423434ac29
Edvaldo Fendi <edvaldo.defendi@gmail.com>
2023-01-19 08:51:42 -0300

    rota de resumo de dias

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -116,7 +116,7 @@ export async function appRoutes(app: FastifyInstance) {
         if (dayHabit) {
             //remover a marcação de completo
             await prisma.dayHabit.delete({
-                where:{
+                where: {
                     id: dayHabit.id,
                 }
             })
@@ -135,10 +135,26 @@ export async function appRoutes(app: FastifyInstance) {
 
 
     app.get('/summary', async () => {
-        const summary = await prisma.$queryRaw `
+        const summary = await prisma.$queryRaw`
         SELECT 
             D.id, 
             D.date, 
+            (
+                SELECT 
+                    cast(count (*) as float)
+                FROM day_habits DH
+                WHERE DH.day_id = D.id
+            )as completed,
+            (
+                SELECT 
+                    cast(count (*) as float)
+                FROM habit_week_days HWD
+                JOIN habits H 
+                    ON H.id = HWD.habit_id
+                WHERE
+                    HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int) 
+                    AND H.created_at <= D.date
+            ) as amount 
         FROM days D
         `
 
